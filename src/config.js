7b6398ca627bf436d35b0a232fa2ee384780d9ce
Navdeep Dhakar <navdepdhakar1@gmail.com>
2022-08-13 19:29:28 +0530

    no cors

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const UNIFY_URI = 'http://localhost:8002';
+export const UNIFY_URI =
+  process.env.NODE_ENV === 'production' ? 'https://apponeapi.herokuapp.com' : 'http://localhost:8002';
 // 'http://localhost:8002'
 // 'https://apponeapi.herokuapp.com';
