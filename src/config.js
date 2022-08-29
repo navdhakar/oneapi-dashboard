99d709fddfd4f3aa55eb50d18ccf1fa3233e953b
navdhakar <navdepdhakar1@gmail.com>
2022-08-29 11:02:39 +0530

    live payments started

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const UNIFY_URI =
   process.env.NODE_ENV === 'production' ? 'https://apponeapi.herokuapp.com' : 'http://localhost:8002';
 // 'http://localhost:8002'
 // 'https://apponeapi.herokuapp.com';
-export const RZR_KEYID = 'rzp_test_2x7BgqQ0pkYHTZ';
+export const RZR_KEYID = 'rzp_live_8Sr5vsNlwBMmQT';
